3648c25d2d8f2200fa3700cd6c8d6db997a04db4
Abhizer Saifee <sabhizer@example.com>
2023-06-12 00:07:51 +0530

    Template Cache

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -3,12 +3,20 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"web-templates/pkg/config"
 	"web-templates/pkg/handlers"
+	"web-templates/pkg/render"
 )
 
 var port = ":8080"
 
 func main() {
+	tc, _ := render.CreateTemplateCache()
+	fmt.Println("Template Cache:", tc)
+
+	app := config.AppConfig{TemplateCache: tc}
+
+	render.NewTemplate(app)
 
 	http.HandleFunc("/", handlers.Home)
 	http.HandleFunc("/about", handlers.About)
--- pkg/render/render.go
+++ pkg/render/render.go
@@ -4,12 +4,64 @@ import (
 	"fmt"
 	"html/template"
 	"net/http"
+	"path/filepath"
+	"web-templates/pkg/config"
 )
 
+var app config.AppConfig
+
+// Getting app instance of struct from main package to render packege.
+// Cant make it public and use as it will give cyclic import errors.
+func NewTemplate(a config.AppConfig) {
+	app = a
+}
+
 func RenderTemplate(w http.ResponseWriter, tmpl string) {
-	parsedTemplate, _ := template.ParseFiles("../../templates/base.layout.tmpl", "../../templates/"+tmpl)
+	parsedTemplate := app.TemplateCache[tmpl]
 	err := parsedTemplate.Execute(w, nil)
 	if err != nil {
 		fmt.Println(err)
 	}
+}
+
+func CreateTemplateCache() (map[string]*template.Template, error) {
+	tc := map[string]*template.Template{}
+
+	layoutfile, err := filepath.Glob("../../templates/*.layout.tmpl")
+	if err != nil {
+		fmt.Println(err)
+		return tc, err
+	}
+	fmt.Println("Layout files:", layoutfile)
+	pagefiles, err := filepath.Glob("../../templates/*.page.tmpl")
+	if err != nil {
+		fmt.Println(err)
+		return tc, err
+	}
+	fmt.Println("Page files:", pagefiles)
+
+	for _, page := range pagefiles {
+		pagekey := filepath.Base(page)
+		fmt.Println("Templating for", pagekey)
+		if len(layoutfile) > 0 {
+			intermediateTemplate, err := template.ParseFiles(page)
+			if err != nil {
+				fmt.Println(err)
+				return tc, err
+			}
+			tc[pagekey], err = intermediateTemplate.ParseGlob("../../templates/*.layout.tmpl")
+			if err != nil {
+				fmt.Println(err)
+				return tc, err
+			}
+		} else {
+			tc[pagekey], err = template.ParseFiles(page)
+			if err != nil {
+				fmt.Println(err)
+				return tc, err
+			}
+		}
+	}
+
+	return tc, nil
 }
